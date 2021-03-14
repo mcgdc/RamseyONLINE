22960ccfc6e591d7dae739a9bcc33880c002e255
Magdalena Łozińska <45043996+mcgdc@users.noreply.github.com>
2021-03-14 18:15:31 +0100

    main window

--- RamseyONLINE/Form1.Designer.cs
+++ RamseyONLINE/Form1.Designer.cs
@@ -29,10 +29,21 @@ namespace RamseyONLINE
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1408, 787);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RamseyONLINE";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
